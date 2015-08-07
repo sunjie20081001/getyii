d4ffec8fd7270bf1b156b59b5cad5ce4c02dc8ac
xiao tu zi <sunjie20081001@126.com>
2015-08-07 12:51:16 +0800

    更新 courese/view

--- frontend/modules/course/views/default/view.php
+++ frontend/modules/course/views/default/view.php
@@ -18,18 +18,17 @@ $this->title = Html::encode($model->title);
 <div class="course-view row">
     <div class="col-md-8">
             <!--视频宣传 start-->
-            <div class="course-vide">
+            <div class="course-video">
                 
             </div>
             <!--视频宣传 end-->
     </div>
-    <div class="col-md4">
+    <div class="col-md-4">
             <!--作者简介 start-->
         <div class="panel panel-default thumbnail center">
-            <img class="img-circle img-responsive" src="<?php?>" alt="">
             <?php echo Html::img($author->getUserAvatar(200),['class'=>'img-circle img-responsive'])?>
-                <h1 class="text-center"><strong>小胖</strong></h1>
-                <p class="text-center"></p>
+                <h1 class="text-center"><strong><?php echo $author->label;?></strong></h1>
+                <p class="text-center"><?php echo $author->tagline;?></p>
         </div>
             <!--作者简介　end-->
     </div>
